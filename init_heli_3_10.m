47d3dae83e21eb8af31fe73cd70bbe9f4b042810
Lars Lien <lars@familienlien.com>
2025-11-04 17:21:46 +0100

    Labb 4, no plots

--- init_heli_3_10.m
+++ init_heli_3_10.m
@@ -73,24 +73,63 @@ F = [K(1,1) K(1,3); K(2,1) K(2,3)];
 poles_plant = eig(A-B*K)
 
 %Defining observer matrices
-A_obs = [0 1 0 0 0;
-        0 0 0 0 0;
-        0 0 0 1 0;
-        0 0 0 0 0;
-        K_3 0 0 0 0];
-B_obs = [ 0 0;
+A_kalman = [0 1 0 0 0 0;
+        0 0 0 0 0 0;
+        0 0 0 1 0 0;
+        0 0 0 0 0 0;
+        0 0 0 0 0 1;
+        K_3 0 0 0 0 0];
+B_kalman = [ 0 0;
         0 K_1;
         0 0;
         K_2 0;
+        0 0;
         0 0];
 
-C_obs = eye(5);
+C_kalman = eye(6);
 
 
-luenberger_poles = [-2,-3,-2,-1.5,-3]
-L_obs =place(A_obs', C_obs',luenberger_poles)'
 
-poles_L = eig(L_obs) %snakke om i rapporten sjef??
+%Discretization:
+Td = 0.002;
+[Ad, Bd] = c2d(A_kalman,B_kalman,Td)
+Cd = C_kalman
 
+%Finding covariances:
+%R_d2 = cov(simout);
+R_d1 = 1.0e-05*[0.0990    0.0017   -0.0047   -0.0094    0.0012   -0.0006;
+    0.0017    0.0921   -0.0263    0.0025    0.0015   -0.0353;
+   -0.0047   -0.0263    0.4135    0.0102    0.0044   -0.0150;
+   -0.0094    0.0025    0.0102    0.2962    0.0006   -0.0115;
+    0.0012    0.0015    0.0044    0.0006    0.0230    0.0027;
+   -0.0006   -0.0353   -0.0150   -0.0115    0.0027    0.0769];
+
+Rd =    [0.0067    0.0009    0.0073    0.0012    0.0013   -0.0002;
+    0.0009    0.0056    0.0014    0.0018   -0.0011    0.0004;
+    0.0073    0.0014    0.0313   -0.0066   -0.0004   -0.0003;
+    0.0012    0.0018   -0.0066    0.0112   -0.0000   -0.0010;
+    0.0013   -0.0011   -0.0004   -0.0000    0.0115    0.0021;
+   -0.0002    0.0004   -0.0003   -0.0010    0.0021    0.0023]
+
+
+Qd = 0.005*[1 0 0 0 0 0;
+       0 1 0 0 0 0;
+       0 0 1 0 0 0;
+       0 0 0 1 0 0;
+       0 0 0 0 1 0;
+       0 0 0 0 0 1];
+%The first covariance is calculated with data when the helicopter is at
+%rest. Second one is at the linearization point while flying, Second one is
+%probably more representative as the first one is basically 0.
+
+%Initical conditions:
+x_0 = [0;
+        0;
+        0;
+        0;
+        0;
+        0];
+
+P_0 = Qd;
 
 PORT = 10;
